8138390e156fa935ba2fa5a2079b8ca7661cb29a
winciu <winciu@gazeta.pl>
2023-12-23 12:28:07 +0100

    initial project files

---
+++ src/main/java/pl/rationalworks/cryptorecommendationservicetest/CryptoRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.rationalworks.cryptorecommendationservicetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoRecommendationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/rationalworks/cryptorecommendationservicetest/CryptoRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rationalworks.cryptorecommendationservicetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
