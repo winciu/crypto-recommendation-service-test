7f83ceac959443b48c1f6463bfa32d956b053f03
winciu <winciu@gazeta.pl>
2023-12-30 00:14:47 -0800

    added Builder pattern to CryptoDailyRecentFactors

--- src/main/java/pl/rationalworks/cryptorecommendationservicetest/model/CryptoDailyRecentFactors.java
+++ src/main/java/pl/rationalworks/cryptorecommendationservicetest/model/CryptoDailyRecentFactors.java
@@ -2,9 +2,9 @@ package pl.rationalworks.cryptorecommendationservicetest.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import pl.rationalworks.cryptorecommendationservicetest.repository.CryptoRecentPriceFactors;
 
 import java.math.BigDecimal;
@@ -82,76 +82,94 @@ import java.time.Instant;
 })
 @Entity
 @Table(name = "daily_recent_factors")
-@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @EqualsAndHashCode
+@Builder
 public class CryptoDailyRecentFactors {
 
     @EmbeddedId
-    private DailyRecentFactorId id;
+    private final DailyRecentFactorId id;
     @Column(name = "min_price", precision = 16, scale = 5)
-    private BigDecimal minPrice;
+    private final BigDecimal minPrice;
     @Column(name = "max_price", precision = 16, scale = 5)
-    private BigDecimal maxPrice;
+    private final BigDecimal maxPrice;
     /**
      * This field stores the oldest currency price we have since the beginning of the current day (id.referenceDate)
      */
     @Column(name = "oldest_price", precision = 16, scale = 5)
-    private BigDecimal oldestPrice;
+    private final BigDecimal oldestPrice;
     @Column(name = "oldest_price_date")
-    private Instant oldestPriceDate;
+    private final Instant oldestPriceDate;
     /**
      * This field stores the newest currency price we have for the current day (id.referenceDate)
      */
     @Column(name = "newest_price", precision = 16, scale = 5)
-    private BigDecimal newestPrice;
+    private final BigDecimal newestPrice;
     @Column(name = "newest_price_date")
-    private Instant newestPriceDate;
-
+    private final Instant newestPriceDate;
     @Column(name = "daily_normalized_factor", precision = 16, scale = 5)
-    private BigDecimal dailyNormalizedFactor;
+    private final BigDecimal dailyNormalizedFactor;
     /**
      * This property stores a normalized weekly factor for a given currency. Here 'weekly' means a week period to date.
      * That means that this factor is calculated using a data from a 7 days back up to now (that is, the current date which is an
      * id.referenceDate).
      */
     @Column(name = "weekly_normalized_factor", precision = 16, scale = 5)
-    private BigDecimal weeklyNormalizedFactor;
+    private final BigDecimal weeklyNormalizedFactor;
     /**
      * This property stores a normalized monthly factor for a given currency. Here 'monthly' means a month period to date.
      * That means that this factor is calculated using a data from a 31 days back up to now (that is, the current date which is an
      * id.referenceDate).
      */
     @Column(name = "monthly_normalized_factor", precision = 16, scale = 5)
-    private BigDecimal monthlyNormalizedFactor;
+    private final BigDecimal monthlyNormalizedFactor;
+
+    public CryptoDailyRecentFactors() {
+        this(null, null, null, null, null,
+            null, null, null, null, null);
+    }
 
     public static CryptoDailyRecentFactors setupDailyEvaluationFactors(DailyRecentFactorId id, BigDecimal minPrice,
                                                                        BigDecimal maxPrice,
                                                                        BigDecimal normalizedFactor) {
-        return new CryptoDailyRecentFactors(id, minPrice, maxPrice, null, null,
-            null, null, normalizedFactor, null, null);
+        return CryptoDailyRecentFactors.builder()
+            .id(id)
+            .minPrice(minPrice)
+            .maxPrice(maxPrice)
+            .dailyNormalizedFactor(normalizedFactor)
+            .build();
     }
 
     public static CryptoDailyRecentFactors setupOldestPriceFactors(DailyRecentFactorId id,
                                                                    BigDecimal oldestPrice, Instant oldestPriceDate) {
-        return new CryptoDailyRecentFactors(id, null, null, oldestPrice, oldestPriceDate, null,
-            null, null, null, null);
+        return CryptoDailyRecentFactors.builder()
+            .id(id)
+            .oldestPrice(oldestPrice)
+            .oldestPriceDate(oldestPriceDate)
+            .build();
     }
 
     public static CryptoDailyRecentFactors setupNewestPriceFactors(DailyRecentFactorId id,
                                                                    BigDecimal newestPrice, Instant newestPriceDate) {
-        return new CryptoDailyRecentFactors(id, null, null, null, null,
-            newestPrice, newestPriceDate, null, null, null);
+        return CryptoDailyRecentFactors.builder()
+            .id(id)
+            .newestPrice(newestPrice)
+            .newestPriceDate(newestPriceDate)
+            .build();
     }
 
     public static CryptoDailyRecentFactors setupNormalizedWeeklyFactors(DailyRecentFactorId id, BigDecimal factorValue) {
-        return new CryptoDailyRecentFactors(id, null,null,null,null,
-            null, null, null, factorValue, null);
+        return CryptoDailyRecentFactors.builder()
+            .id(id)
+            .weeklyNormalizedFactor(factorValue)
+            .build();
     }
 
     public static CryptoDailyRecentFactors setupNormalizedMonthlyFactors(DailyRecentFactorId id, BigDecimal factorValue) {
-        return new CryptoDailyRecentFactors(id, null, null, null, null,
-            null, null, null, null, factorValue);
+        return CryptoDailyRecentFactors.builder()
+            .id(id)
+            .monthlyNormalizedFactor(factorValue)
+            .build();
     }
 }
